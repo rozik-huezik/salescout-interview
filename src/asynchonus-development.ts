2ab4daddf2e0568c3f8322e87784f2caf8fe51b8
rozik-huezik <vikanovaroza@gmail.com>
2025-03-20 13:15:43 +0500

    Update asynchonus-development.ts

--- src/asynchonus-development.ts
+++ src/asynchonus-development.ts
@@ -1,24 +1,23 @@
-// Write a function that accepts an array of URLs,
-// makes parallel queries for each of them, and returns an
-// an array of results in the order in which the queries are completed.
+async function fetchAll(urls) {
+    const requests = urls.map(async (url) => {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return {
+                data,
+                status: response.status
+            };
+        } catch (error) {
+            return {
+                data: error.message,
+                status: 500
+            };
+        }
+    });
 
-// Example input data:
-// const urls = ['https://jsonplaceholder.typicode.com/posts/1', 
-// 'https://jsonplaceholder.typicode.com/posts/2'];
-
-// Expected result:
-// [
-// { data: { ... }, status: 200 },
-// { data: { ... }, status: 200 }
-// ] 
-type RequestsResult = {
-    data: any,
-    status: number
+    return Promise.all(requests);
 }
 
-async function fetchAll(urls: string[]): Promise<RequestsResult[]> {
-    //Your code goes here
-    return [];
-}
+module.exports = { fetchAll };
 
 module.exports = { fetchAll };
